0cf87f12ed19db00911dcc5ca5bf935729d5190f
Amit Dubey <amitdubey8888@gmail.com>
2018-04-05 00:27:40 +0530

    build

--- client/src/app/providers/country.service.ts
+++ client/src/app/providers/country.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
@@ -13,18 +13,3 @@ export class CountryService {
         .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
 }
-
-// <input type="text" name="pincode" class="form-control" (keyup)="onKey($event)" [ngModel]="pincode">
-// <input type="text" name="state" class="form-control" value="{{state}}" [ngModel]="state">
-// <input type="text" name="country" class="form-control" value="{{country}}" [ngModel]="country">
-
-// onKey(event: any) {
-//     this.countryService.getComments(event.target.value)
-//     .subscribe(
-//       country_name => this.country_name = country_name,
-//         err => {
-//             console.log(err);
-//       });
-//     // this.state = this.country_name.results[0].address_components[1].long_name;
-//     // this.country = this.country_name.results[0].address_components[2].long_name;
-// }
